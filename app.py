02470e7e63e8da3a606d9c4711a53ff8bafab18a
RAJAMANI01 <srarajamani2007@gmail.com>
2026-06-09 11:38:25 +0530

    Initial project upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 # Load dataset
-df = pd.read_csv("twitter_training.csv", header=None)
+df = pd.read_csv("twitter_training.csv//twitter_training.csv", header=None)
 df.columns = ['ID', 'Topic', 'Sentiment', 'Text']
 
 # Clean text
